53b0d1c7fb3f1437e79def48295c8656944ca813
Ryan Hoofard <rahoofard@mail.csuchico.edu>
2022-04-06 18:22:32 -0700

    basic node and express web app without docker

---
+++ myapp/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+	res.json({
+    		"message": "My name is Ryan Hoofard",
+    		"timestamp": (new Date()).getTime()
+	});
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
